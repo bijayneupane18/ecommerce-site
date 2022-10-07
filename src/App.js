1ca3aa0446d36fdc0bc19ded5c3cca6677cb254b
bijayneupane18 <bjneupane5@gmail.com>
2022-10-07 23:06:03 +0545

    half of header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import CardsDetails from "./components/CardsDetails";
+import Cards from "./components/Cards";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
-      <Header/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/cart" element={<CardsDetails />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge";
+import { NavLink } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <Navbar bg="dark" variant="dark" style={{ height: "60px" }}>
+        <Container>
+          <NavLink to="/" className="text-decoration-none text-light mx-3">
+            Add to Cart
+          </NavLink>
+          <Nav className="me-auto">
+            <NavLink to="/">Home</NavLink>
+          </Nav>
+          <Badge badgeContent={2} color="primary">
+            <ShoppingCartIcon
+              fontSize="large"
+              style={{ color: "white", cursor: "pointer" }}
+            />
+          </Badge>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default Header
+export default Header;
